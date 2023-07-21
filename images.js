f9cb758b2e7bee58d041b2cd31fcd02a4618c1ec
waithakakingalynette <warukiralynette@gmail.com>
2023-07-21 09:36:21 +0300

    created a visionary visit to a park using javascript

--- images.js
+++ images.js
@@ -1,15 +1,15 @@
 const videoContainers = document.querySelectorAll('.video-container');
 
-    videoContainers.forEach(container => {
-        const image = container.querySelectorAll('img');
-        const video = container.querySelectorAll('video');
+videoContainers.forEach(container => {
+    const video = container.querySelector('video');
 
-        image.addEventListener('click', () => {
-            video.play();
-        });
-
-        image.addEventListener('mouseout', () => {
-            video.pause();
-            video.currentTime = 0;
-        });
+    video.addEventListener('click', () => {
+        video.play();
     });
+
+    video.addEventListener('ended', () => {
+        video.pause();
+        video.currentTime = 0;
+    });
+});
+
